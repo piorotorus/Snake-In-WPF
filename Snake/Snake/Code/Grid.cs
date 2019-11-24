5ce0e1c51c15c4fee63c64701360e7070e17032d
Kamil Jamroz <nergydev@empyrean.games>
2019-11-24 15:49:18 +0100

    added Grid class

---
+++ Snake/Snake/Code/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Code
+{
+    class Grid
+    {
+        uint sideCellCount;
+
+        public Grid(uint sideCellCount)
+        {
+            this.sideCellCount = sideCellCount;
+        }
+    }
+}
