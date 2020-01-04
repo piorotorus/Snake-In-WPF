d2c70d5471f45421461ddb53078c12f43096552a
Kamil Jamroz <nergydev@empyrean.games>
2020-01-04 14:40:20 +0100

    snakegame test

--- Snake/SnakeTestProject/SnakeGameTest.cs
+++ Snake/SnakeTestProject/SnakeGameTest.cs
@@ -7,13 +7,34 @@ namespace SnakeTestProject
     [TestClass]
     public class SnakeGameTest
     {
+        bool GridContainsExactlyOneApple()
+        {
+            int foundApplesCount = 0;
+            var p = new Position { X = 0, Y = 0 };
+
+            for (int y = 0; y < GameState.sideCellCount; y++) {
+                p.Y = y;
+
+                for (int x = 0; x < GameState.sideCellCount; x++) {
+                    p.X = x;
+                    if (GameState.grid.ContainsAt(p, CellContent.Apple)) {
+                        foundApplesCount++;
+                        break;
+                    }
+                }
+            }
+
+            return foundApplesCount == 1;
+        }
+
         [TestMethod]
         public void StartGameTest()
         {
+            Assert.IsFalse(GridContainsExactlyOneApple());
+
             SnakeGame.StartGame();
             Assert.IsTrue(GameState.score == 0);
-
-
+            Assert.IsTrue(GridContainsExactlyOneApple());
         }
     }
 }
