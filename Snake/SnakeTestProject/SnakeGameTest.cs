8681c31a67d4748510c83c9c92009a77ea88ec07
Kamil Jamroz <nergydev@empyrean.games>
2020-01-04 15:14:49 +0100

    moar tests

--- Snake/SnakeTestProject/SnakeGameTest.cs
+++ Snake/SnakeTestProject/SnakeGameTest.cs
@@ -7,7 +7,7 @@ namespace SnakeTestProject
     [TestClass]
     public class SnakeGameTest
     {
-        bool GridContainsExactlyOneApple()
+        int GetGridContentCount(CellContent contentToCount)
         {
             int foundApplesCount = 0;
             var p = new Position { X = 0, Y = 0 };
@@ -17,24 +17,83 @@ namespace SnakeTestProject
 
                 for (int x = 0; x < GameState.sideCellCount; x++) {
                     p.X = x;
-                    if (GameState.grid.ContainsAt(p, CellContent.Apple)) {
+                    if (GameState.grid.ContainsAt(p, contentToCount)) 
                         foundApplesCount++;
-                        break;
-                    }
                 }
             }
 
-            return foundApplesCount == 1;
+            return foundApplesCount;
         }
 
         [TestMethod]
-        public void StartGameTest()
+        public void InitializeTest()
         {
-            Assert.IsFalse(GridContainsExactlyOneApple());
+            Assert.IsNull(GameState.grid);
+            Assert.IsNull(GameState.snake);
+
+            SnakeGame.Initialise();
+
+            Assert.IsNotNull(GameState.grid);
+            Assert.IsNotNull(GameState.snake);
+        }
+
+        [TestMethod]
+        public void StartGameTest() {
+            SnakeGame.Initialise();
+
+            var appleCount = GetGridContentCount(CellContent.Apple);
+            Assert.IsTrue(appleCount == 0);
 
             SnakeGame.StartGame();
+
+            appleCount = GetGridContentCount(CellContent.Apple);
             Assert.IsTrue(GameState.score == 0);
-            Assert.IsTrue(GridContainsExactlyOneApple());
+            Assert.IsTrue(appleCount == 1);
+        }
+
+        [TestMethod]
+        public void PlantTest()
+        {
+            SnakeGame.Initialise();
+
+            int expectedAppleCount = 0;
+            int expectedSpikeCount = 0;
+
+            var appleCount = GetGridContentCount(CellContent.Apple);
+            var spikeCount = GetGridContentCount(CellContent.Spikes);
+
+            Assert.IsTrue(appleCount == expectedAppleCount);
+            Assert.IsTrue(spikeCount == expectedSpikeCount);
+
+            for (int i = 0; i < 10; i++) {
+                if (i % 3 == 0) {
+                    SnakeGame.Plant(CellContent.Apple);
+                    appleCount = GetGridContentCount(CellContent.Apple);
+                    expectedAppleCount++;
+                } else {
+                    SnakeGame.Plant(CellContent.Spikes);
+                    spikeCount = GetGridContentCount(CellContent.Spikes);
+                    expectedSpikeCount++;
+                }
+
+                Assert.IsTrue(appleCount == expectedAppleCount);
+                Assert.IsTrue(spikeCount == expectedSpikeCount);
+            }
+        }
+
+        [TestMethod]
+        public void CellContainsSomethingOrSnakeIsThereTest()
+        {
+            SnakeGame.Initialise();
+
+            var appleCount = GetGridContentCount(CellContent.Apple);
+            Assert.IsTrue(appleCount == 0);
+
+            SnakeGame.StartGame();
+
+            appleCount = GetGridContentCount(CellContent.Apple);
+            Assert.IsTrue(GameState.score == 0);
+            Assert.IsTrue(appleCount == 1);
         }
     }
 }
